a6c41d1ced17d1ac0fc0cd12d960fbf4ea1876c1
TobyMechant <thibaut.roux89@gmail.com>
2017-05-18 11:24:49 +0200

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -5,6 +5,7 @@
 		['GET', '/contact', 'Default#contact', 'default_contact'],
 		['GET', '/cgv', 'Default#cgv', 'default_cgv'],
 		['GET', '/gallery', 'Default#gallery', 'default_gallery'],
+		['GET', '/events', 'Default#events', 'default_events'],
 		['GET', '/admin', 'Admin#dashboard', 'admin_dashboard'],
 		['GET|POST', '/admin/ajout-article', 'Blog#addArticle', 'blog_addArticle'],
 		['GET', '/admin/articles', 'Blog#listArticles', 'blog_listArticles'],
