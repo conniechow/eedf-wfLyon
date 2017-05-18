2c846d353bc0843d131fc929897515df211c5233
TobyMechant <thibaut.roux89@gmail.com>
2017-05-18 11:12:25 +0200

    Update event.php

--- app/Views/default/event.php
+++ app/Views/default/event.php
@@ -1,6 +1,8 @@
 <?php $this->layout('layout', ['title' => 'Events']) ?>
 
 <?php $this->start('main_content') ?>
-	<h2>Prochaines Sortie:</h2>
+	<h2>Calendrier:</h2>
+	<iframe src="https://calendar.google.com/calendar"></iframe>
+	<h2>Prochaines Sorties:</h2>
 	<p></p>
 <?php $this->stop('main_content') ?>
