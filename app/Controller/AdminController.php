f758629b3928534c879024681d508bb19ff5f7d7
Guillermo <gonzalezdecastro.guillermo@gmail.com>
2017-05-19 12:25:34 +0200

    Account Creation
    Checks if email exists. Sends email to the new user.

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -5,15 +5,18 @@ namespace Controller;
 use \W\Controller\Controller;
 use \W\Security\AuthentificationModel;
 use \W\Model\UsersModel;
+//use \vendor\phpmailer\phpmailer\PHPMailer;
 
 class AdminController extends Controller {
 
   protected $currentUser;
   protected $auth;
+  protected $mail;
 
   public function __construct(){
     $this->currentUser = new UsersModel;
-    $this->auth = new AuthentificationModel;
+    $this->auth        = new AuthentificationModel;
+    $this->mail        = new \PHPMailer();
   }
 
   public function dashboard(){
@@ -23,11 +26,18 @@ class AdminController extends Controller {
     if($_SERVER['REQUEST_METHOD'] == 'GET'){
       $this->show('user/inscription');
     }else{
-      $_POST['password'] = $this->auth->hashPassword($_POST['password']);
-      $_POST['role'] = 'admin';
-      $newUser = $this->currentUser->insert($_POST);
-      $this->auth->logUserIn($newUser);
-      $this->redirectToRoute('default_home');
+      if($this->currentUser->emailExists($_POST['email'])){
+        //$this->show('dev/output',['result'=>'il existe deja']);
+        $this->show('user/inscription',['error'=>'emailExists']);
+      }else{
+        $_POST['password'] = $this->auth->hashPassword($_POST['password']);
+        $_POST['role'] = 'admin';
+        $newUser = $this->currentUser->insert($_POST);
+        $this->auth->logUserIn($newUser);
+        //$this->redirectToRoute('default_home');
+        $isSentEmail = $this->sendEmail($_POST['email']);
+        $this->show('dev/output',['result'=>'email sent']);
+      }
     }
   }
 
@@ -51,9 +61,29 @@ class AdminController extends Controller {
     $this->redirectToRoute('default_home');
   }
 
-
   public function login(){
     $this->show('admin/login');
+  }
+
+  private function sendEmail($address){
+    $this->mail->isSMTP();
+    $this->mail->isHTML(true);
+    $this->mail->Host = "smtp.gmail.com";
+    $this->mail->Port = 465;
+    $this->mail->SMTPAuth = true;
+    $this->mail->SMTPSecure = 'ssl';
+    $this->mail->Username  = "wf3lyon@gmail.com";
+    $this->mail->Password = "Azerty1234";
+    $this->mail->SetFrom('wf3lyon@gmail.com','BioForce3 Lyon');
+    $this->mail->addAddress($address);
+    $this->mail->Subject = 'EEDF Validation d\'email';
+    $bodyContent = '<p>Hola! Je suis EEDF</p>';
+    $this->mail->Body = $bodyContent;
+    if (!$this->mail->send()) {
+        return "Mailer Error: " . $this->mail->ErrorInfo;
+    } else {
+        return "Message sent!";
+    }
   }
 }
 
