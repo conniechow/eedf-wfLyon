a28855478704bd65e0f183be57bb3b05c142ef31
TobyMechant <thibaut.roux89@gmail.com>
2017-05-19 11:15:21 +0200

    Update eventsLayout.php

--- app/Views/eventsLayout.php
+++ app/Views/eventsLayout.php
@@ -11,16 +11,30 @@
 			<h1>Calendrier/Evénements</h1>
 		</header>
 		<section>
-		  <?= $this->section('main_content') ?>
-		  <iframe></iframe>
-    </section>
-    <section>
-		  <?= $this->section('main_content') ?>
-		    <--! Afficher Evénements !-->
-    </section>
+			<?= $this->section('main_content') ?>
+			<iframe></iframe>
+    		</section>
+    		
+		<section>
+		  	<?= $this->section('main_content') ?>
+			<p>Evénement du mois: Sausage Party :D</p>
+			    
+    		</section>
 		<footer><?= date('Y') ?> W</footer>
 	</div>
 </body>
 </html>
 
-	
+<?php 
+//hérite du fichier layout.php à la racine de app/Views/
+$this->layout('layout')
+?>
+
+<?php 
+//début du bloc main_content
+$this->start('main_content'); ?>
+<h1>Prochains Evénements</h1>
+
+<?php 
+//fin du bloc
+$this->stop('main_content'); ?>
