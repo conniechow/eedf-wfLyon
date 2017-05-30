69979e2efda8886b18157b213851109182207cf4
Guillermo <gonzalezdecastro.guillermo@gmail.com>
2017-05-30 15:32:15 +0200

    Update manageUsers.php

--- app/Views/admin/manageUsers.php
+++ app/Views/admin/manageUsers.php
@@ -1,36 +1,23 @@
-<?php $this->layout('layout-admin',['title'=>'Dashboard']); ?>
+<?php $this->layout('layout-user-management',['title'=>'Dashboard']); ?>
 <?php $this->start('main_content') ?>
+<h2>Index</h2>
+<ul>
+  <li><a href="<?= $this->url('default_home'); ?>">Accueil</a></li>
+  <li><a href="<?= $this->url('userManagement_inscription'); ?>">Inscription</a></li>
+  <li><a href="<?= $this->url('userManagement_list_users'); ?>">Lister Users</a></li>
+  <li><a href="<?= $this->url('userManagement_list_admins'); ?>">Lister Admins</a></li>
+  <li><a href="<?= $this->url('userManagement_add_user_admin_form'); ?>">Ajouter Admins</a></li>
+</ul>
 
-<h2>Manage Users</h2>
-<p><?php
-if(isset($error)){
-  print $error;
-}
-?></p>
-<p><?php
-if(isset($email)){
-  print $email;
-}
-?></p>
-<p><?php
-if(isset($password)){
-  print $password;
-}
-?></p>
-<h3>Logged User:</h3>
-<p><?php
-if(isset($logged)){
-  var_dump($logged);
-}
-?></p>
-<p>
 <?php if(isset($user)): ?>
   <p>Salut, <?php print $user['username'] ?></p>
+  <p><a href="<?= $this->url('admin_deconnexion');  ?>">deconnexion</a></p>
 <?php endif ?>
 </p>
+
 <!-- LIST ADHERENTS -->
 
-<?php if($w_current_route == 'userManagement_list'):?>
+<?php if($w_current_route == 'userManagement_list_users'):?>
 <h3>Users list:</h3>
 <table>
   <tr>
@@ -39,15 +26,13 @@ if(isset($logged)){
     <th>phone</th>
     <th>actions</th>
   </tr>
-  <?php foreach ($usersList as $key => $value): {
-    print '<tr>';
-    print '<td>'.$value['username'].'</td>';
-    print '<td>'.$value['email'].'</td>';
-    print '<td>'.$value['phone'].'</td>';
-    print '<td><a href="'.$this->url('userManagement_delete_user',['id'=>$value['id']]).'">Delete</a></td>';
-    print '</tr>';
-  } ?>
-<?php endforeach ?>
+  <?php foreach ($usersList as $key => $value): ?>
+    <tr>
+      <td><?= $value['username'] ?></td>
+      <td><?= $value['email'] ?></td>
+      <td><?= $value['phone'] ?></td>
+      <td><a href="<?= $this->url('userManagement_delete_user',['id'=>$value['id']]) ?>">Delete</a></td>
+  <?php endforeach ?>
 <?php endif ?>
 </table>
 
@@ -73,6 +58,9 @@ if(isset($logged)){
 <?php endif ?>
 </table>
 
+<!-- ADD ADMINS -->
+
+<?php if($w_current_route == 'userManagement_add_user_admin_form'):?>
 <h3>Ajouter un admin</h3>
 <form class="" action="<?= $this->url('userManagement_add_user_admin') ?>" method="post">
   <label for="username">Nom</label>
@@ -83,5 +71,5 @@ if(isset($logged)){
   <input type="text" name="password" value="">
   <input type="submit" name="" value="Ajouter Admin">
 </form>
-
+<?php endif ?>
 <?php $this->stop('main_content') ?>
