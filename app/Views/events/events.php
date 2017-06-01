5a1a21116dc83fbd397ec1ce9d62794ff962ed9e
TobyMechant <thibaut.roux89@gmail.com>
2017-06-01 14:39:10 +0200

    maj events

--- app/Views/events/events.php
+++ app/Views/events/events.php
@@ -44,11 +44,11 @@
 
       // On récupère tout le contenu de la table events
       $reponse = $bdd->query('SELECT * FROM events');
-      var_dump($reponse);
+      //var_dump($reponse);
 
         while($donnees = $reponse->fetch()){ ?>
                 <?php if($donnees['idevent'] < 4){ ?>
-                      <?php var_dump($donnees); ?>
+
                         <div class="container">
                             <p>
                               <strong>Sortie : "</strong>  <?php echo $donnees['title']; ?><strong> "</strong><br />
@@ -58,7 +58,6 @@
                             </p>
                         <div>
                 <?php }else{ ?>
-                  <?php var_dump($donnees); ?>
 
                   <div id="event" class="container hidden">
                       <p>
