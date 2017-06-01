3047e210a3295ff4994d0fddf36b0bcd58c1d223
Connie <cest.moi.ladyc@gmail.com>
2017-06-01 23:26:13 +0200

    Change of DB config

--- app/config.dist.php
+++ app/config.dist.php
@@ -1,11 +1,11 @@
-<?php
+﻿<?php
 
 $w_config = [
    	//information de connexion à la bdd
 	'db_host' => 'localhost',						//hôte (ip, domaine) de la bdd
-    'db_user' => 'root',							//nom d'utilisateur pour la bdd
-    'db_pass' => '',								//mot de passe de la bdd
-    'db_name' => '',								//nom de la bdd
+    'db_user' => 'admineedf',							//nom d'utilisateur pour la bdd
+    'db_pass' => 'scoutForever2017',								//mot de passe de la bdd
+    'db_name' => 'wf3lyon',								//nom de la bdd
     'db_table_prefix' => '',						//préfixe ajouté aux noms de table
 
 	//authentification, autorisation
