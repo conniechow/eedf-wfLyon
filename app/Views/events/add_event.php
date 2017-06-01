3310f1ed08c6f08a086862c1f8f4adb2107e289a
TobyMechant <thibaut.roux89@gmail.com>
2017-06-01 15:54:13 +0200

    maj add_event
    test ok :dart:

--- app/Views/events/add_event.php
+++ app/Views/events/add_event.php
@@ -5,7 +5,7 @@
 <h3 id="titreElem"> Ajout d'événement</h3>
 <div id="formulaireElem">
   <div class="col-md-3"></div>
-  <form class="col-md-6 jumbotron" action="<?= $this->url('event_add_event') ?>" method="post" enctype="multipart/form-data" id="ajoutElemForm">
+  <form class="col-md-6 jumbotron" action="<?= $this->url('events_add_event') ?>" method="post" enctype="multipart/form-data" id="ajoutElemForm">
       <div class="form-group text-center" id="formElem2">
           <label for="elemName" class="control-label text-center">Titre de l'événement</label>
           <input type="text" class="form-control" name="elemName" id="elemName" value="" required>
