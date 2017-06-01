de16801c1305a9ca4ee1cdbeb0143186f0ba09ff
TobyMechant <thibaut.roux89@gmail.com>
2017-06-01 15:12:49 +0200

    maj

--- app/Views/events/add_event.php
+++ app/Views/events/add_event.php
@@ -9,30 +9,28 @@
       <div class="form-group text-center" id="formElem2">
           <label for="elemName" class="control-label text-center">Titre de l'événement</label>
           <input type="text" class="form-control" name="elemName" id="elemName" value="" required>
-    </div>
+      </div>
 
-    <div class="form-group text-center" id="startdate">
-      <label for="startdate" class="control-label text-center">Date de début</label>
-      <input type="date" class="form-control" name="startdate" name="startdate" value="<?= date('Y-m-d h:i:s'); ?>" required>
-    </div>
-    <div class="form-group text-center" id="enddate">
-        <label for="enddate" class="control-label text-center">Date de fin</label>
-        <input type="date" class="form-control" name="enddate" name="enddate" value="<?= date('Y-m-d h:i:s'); ?>" required>
-    </div>
+      <div class="form-group text-center" id="startdate">
+          <label for="startdate" class="control-label text-center">Date de début</label>
+          <input type="date" class="form-control" name="startdate" name="startdate" value="<?= date('Y-m-d h:i:s'); ?>" required>
+      </div>
+      <div class="form-group text-center" id="enddate">
+          <label for="enddate" class="control-label text-center">Date de fin</label>
+          <input type="date" class="form-control" name="enddate" name="enddate" value="<?= date('Y-m-d h:i:s'); ?>" required>
+      </div>
 
-    <div class="form-group text-center">
+      <div class="form-group text-center">
           <label for="elemDescription" class="control-label">Description de l'événement</label>
           <textarea id="elemDescription" class="form-control" name="elemDescription"></textarea>
-    </div>
+      </div>
 
-
-
-    <div class="form-group text-center">
+      <div class="form-group text-center">
           <div class="">
               <input type="hidden" name="date" value="<?= date('Y-m-d h:i:s'); ?>">
               <button type="submit" class="btn btn-success" id="btnadddoc">Ajouter</button>
-        </div>
-  </div>
+          </div>
+      </div>
   </form>
   </div>
 </div>
