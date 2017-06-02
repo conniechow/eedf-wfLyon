27cd33b522f33749942992639171c8491a5b8c67
conniebot <chow.connie.lyon69@gmail.com>
2017-06-02 10:48:11 +0200

    Photo model renaming

--- app/Model/PhotosModel.php
+++ app/Model/PhotosModel.php
@@ -9,16 +9,16 @@ class PhotosModel extends \W\Model\Model{
 	public function add_photos($args){
 		
 		/* Récupération de la photo */
-		if(isset($_FILES['docfile'])){
-			$repertoire = 'documents/'; // le répertoire de destination de l'image
-			$args['docfilename'] = $_FILES['docfile']['name']; // le nom de la photo
-			$fichier = $this->slugify($_FILES['docfile']['name']); // le nom de la photo slugifié
-			$tmpName = $_FILES['docfile']['tmp_name']; // le nom provisoire
-			$args['size'] = $_FILES['docfile']['size']; // taille du fichier
+		if(isset($_FILES['photofile'])){
+			$repertoire = 'photos/'; // le répertoire de destination de l'image
+			$args['photofilename'] = $_FILES['photofile']['name']; // le nom de la photo
+			$fichier = $this->slugify($_FILES['photofile']['name']); // le nom de la photo slugifié
+			$tmpName = $_FILES['photofile']['tmp_name']; // le nom provisoire
+			$args['size'] = $_FILES['photofile']['size']; // taille du fichier
 			// déplacement
 			if(move_uploaded_file($tmpName, 'assets/'.$repertoire.$fichier))
 			{
-				$args['docfile'] = $repertoire.$fichier;
+				$args['photofile'] = $repertoire.$fichier;
 				$this->insert($args);
 			}
 		}
