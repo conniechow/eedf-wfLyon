50c42ca54eb65761e4b48cbf86ca0ffe9d8ee454
conniebot <chow.connie.lyon69@gmail.com>
2017-06-02 15:02:36 +0200

    photo controller update

--- app/Views/photo/photos.php
+++ app/Views/photo/photos.php
@@ -10,19 +10,12 @@
 	<div><!-- Dynamic display of photos -->
 		
 	<h4>Photos</h4>
-	<img src="<?= $data[0] ?>" width="150" height="auto"/>
-  <?php for($i=0;$i++;$i<51){
-    print $i;
-  	}
+	
+	<?php foreach ($photos as $key => $photo) {
+		if($photo['id_categorie'] == '1'){ 
+		}
+	}?>
 
-  for ($i=1; $i < 51 ; $i++) {
-    $numero = $i;
-    if($numero<10){
-      $numero = '0'.$numero;
-    }
-   	print '<img src="'.$path.$data.$numero.'.jpg" width="150" height="auto"/>';
-  	} ?>
-
-	</div>	
+	</div>
 		
 <?php $this->stop('main_content') ?>
