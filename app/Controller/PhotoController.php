493a2705700f22b9c2458dd879e363d817b8fe4a
Connie <cest.moi.ladyc@gmail.com>
2017-06-03 09:25:07 +0200

    Updated Photo Controller (pre-cleanup though)

--- app/Controller/PhotoController.php
+++ app/Controller/PhotoController.php
@@ -4,6 +4,7 @@ namespace Controller;
 
 use \W\Controller\Controller;
 use \Model\PhotosModel;
+
 class PhotoController extends Controller {
 
   protected $photosModel;
@@ -12,15 +13,37 @@ class PhotoController extends Controller {
     $this->photosModel = new PhotosModel;
   }
 
+public function photos(){
+ 	$data = $this->PhotosModel->findAll();
+ 	//$this->allowTo('admin'); // seulement visible par l'admin
+	$this->show('photo/photos', ['photos' => $data]);
+	}
 
-  public function showPhotos(){
+	// public function ajaxGetPhoto(){
+	// 	$data = $this->PhotosModel->findAll();
+	// 	foreach ($data as $key => $photo) {
+	// 		$data[$key]['action'] = $this->generateUrl('photo_edit_photos', ['id' => $photo['id']]);
+	// 	}
+	// 	$this->showJson($data);
+	// }
+
+public function showPhotos(){
     $data = $this->photosModel->findAll();
     //$this->allowTo('admin'); // seulement visible par l'admin
     $this->show('photo/photos', ['photos' => $data]);
   }
 
+public function show_photos(){
+	if(is_numeric($id)){
+		$photo = $this->PhotosModel->find($id);
+		}else{
+			$photo = $this->PhotosModel->search($id);
+			$photo = $photo[0];
+		}
+		$this->show('document_documents', ['documents' => $document]);
+	}
 
-  public function addPhotos(){
+public function addPhotos(){
     // Test code for add
     //$this->allowTo('admin'); // seulement visible par l'admin
     if($_SERVER['REQUEST_METHOD'] == 'GET'){
@@ -30,6 +53,37 @@ class PhotoController extends Controller {
       $this->redirectToRoute('photo_photos');
     }
 
+public function add_photos(){
+	//$this->allowTo('admin'); // seulement visible par l'admin
+		if($_SERVER['REQUEST_METHOD'] == 'GET'){
+			$this->show('photo/add_photos');
+		}else{
+            // $this->PhotosModel->insert($_POST);
+            // $this->redirectToRoute('photo_photos');                                        
+            $this->PhotosModel->add_photos($_POST);
+            $this->redirectToRoute('photo_photos');
+
+		}
+	}
+
+	public function edit_photos($id){
+		//$this->allowTo('admin');
+		if($_SERVER['REQUEST_METHOD'] == 'GET'){
+			$photo = $this->PhotosModel->find($id);
+			$this->show('photo/edit_photos', ['photos' => $photo]);
+		}else{
+			$this->PhotosModel->update_photos($_POST, $id);
+			$this->redirectToRoute('photo_photos');
+		}
+	}
+
+	public function delete_photos($id){
+		$photo = $this->PhotosModel->find($id);
+		$this->PhotosModel->delete($id);
+		$chemindoc = '../public/assets/'.$photo['photofile'];
+		unlink($chemindoc); /* suppression de la photo */
+		$this->redirectToRoute('photo_photos');
+	}
 
   }
 }
